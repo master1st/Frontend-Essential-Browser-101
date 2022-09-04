02beb6bd2b8f9f80e0dcb00940dc97634294c2f5
seokhyun <gah1234@naver.com>
2022-09-04 16:57:00 +0900

    dom browser event.clientX

--- coordinate/main.js
+++ coordinate/main.js
@@ -3,6 +3,7 @@
 const target = document.querySelector('.target');
 const widthLine = document.querySelector('.widthLine');
 const heightLine = document.querySelector('.heightLine');
+const coordtext = document.querySelector('.coordtext');
 
 console.log(widthLine.clientTop);
 widthLine.style.top = `${widthLine.clientTop}`;
@@ -15,10 +16,14 @@ heightLine.style.left = `${widthLine.clientLeft}`;
 document.addEventListener('mousemove', (event) => {
     const x = event.clientX;
     const y = event.clientY;
-
+    coordtext.style.top = `${y}px`;
+    coordtext.style.left = `${x}px`;
+    coordtext.innerHTML = `${x}px, ${y}px`;
+    coordtext.style.color = "white";
     target.style.top = `${y}px`;
     target.style.left = `${x}px`;
     widthLine.style.top= `${y}px`;
     heightLine.style.left= `${x}px`;
+
     
 })
