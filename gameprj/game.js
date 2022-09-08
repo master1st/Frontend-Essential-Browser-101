d0c9c7d503583419b0753e593c8345dc37cd864b
seokhyun <gah1234@naver.com>
2022-09-08 16:13:50 +0900

    timer & unit cnt

--- gameprj/game.js
+++ gameprj/game.js
@@ -9,16 +9,27 @@ const fieldRect = unit_field.getBoundingClientRect();
 // const carrot = document.querySelector('carrot');
 // const bug = document.querySelector('bug');
 // 나중에 startstopcnt 리팩토링
+var childbugcnt = 0;
+var unit_count = 10;
+var unitSize = 70;
 var startstopcnt = 0;
 var timercnt = 11;
 var gamestatus = "stop";
 var playtimer;
-function gameStart() {
-    // unit_count();
+var image;
+count.innerHTML = 0;
+
+
+function initGame() {
   startstopcnt += 1;
   if (startstopcnt % 2 == 0) {
-    unit_create("carrot_unit", "/carrot/img/carrot.png", 10);
-    unit_create("bug_unit", "/carrot/img/bug.png", 10)
+    unit_create("carrot_unit", "/carrot/img/carrot.png", unit_count);
+    unit_create("bug_unit", "/carrot/img/bug.png", unit_count);
+    unit_count_init();
+    document.addEventListener('click', (e) => {
+       unit_remove(e);
+    });
+
     fa_solid.setAttribute("class", "fa-solid fa-stop");
     playtimer = setInterval(() => {
       if (timercnt > 0) {
@@ -30,42 +41,54 @@ function gameStart() {
   } else {
     fa_solid.className = "fa-solid fa-play";
     clearInterval(playtimer);
-    //여기에 html 성공메시지 
-    return ;
   }
 }
+
+
+
+startbtn.addEventListener("click", (event) => {
+    initGame();
+  });
+
 
 function unit_create(className, url, count ){
     const x1 = 0;
     const y1 = 0;
-    const x2 = fieldRect.width;
-    const y2 = fieldRect.height;
+    const x2 = fieldRect.width-unitSize;
+    const y2 = fieldRect.height-unitSize;
     for(let i=0; i<count; i++){
-       var image = new Image();
+       image = new Image();
        image.src = url;
        image.setAttribute('class',`${className}`);
        image.style.position = "absolute";
        const x = randomNumber(x1, x2);
        const y = randomNumber(y1, y2);
-       console.log(`x: ${x}, y: ${y}`);
        image.style.top = `${y}px`;
        image.style.left = `${x}px`;
        unit_field.appendChild(image);
     }
 }
-
+function unit_count_init() {
+    unit_field.childNodes.forEach((item) => {
+        if(item.className === 'bug_unit'){
+         childbugcnt += 1;
+        } 
+     });
+     count.innerHTML = childbugcnt;
+};
+function unit_remove(e) {
+    if(e.target.className === "bug_unit"){
+        unit_field.removeChild(e.target);
+        count.innerHTML = childbugcnt-=1; 
+    }
+};
 function randomNumber(min, max) {
     return Math.random() * (max-min) + min;
 }
-
-startbtn.addEventListener("click", (event) => {
-  gameStart();
-});
 function initTimer() {
   ten_digit.InnerText("1");
 }
 function gameTimer() {
-  console.log("1");
   timercnt--;
   if(timercnt>=10){
   timer.innerHTML = `00:${timercnt}`;
